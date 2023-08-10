b301370ff199215732959d92271a80d774af0748
Santiago Ballerini <sballerini@itba.edu.ar>
2023-08-10 17:16:55 -0300

    feat: add drawing script for ej2b

---
+++ draw/ej2-b-draw.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csv_file = "data/ej2-b.csv"
+data = pd.read_csv(csv_file)
+
+hp_values = data["hp"]
+average_values = data["catch_rate"]
+std_dev_values = data["std"]
+
+plt.errorbar(
+    hp_values,
+    average_values,
+    yerr=std_dev_values,
+    fmt="o",
+    label="Catch Rate",
+    capsize=5,
+)
+plt.xlabel("HP")
+plt.ylabel("Average Catch Rate")
+plt.title("Average Catch Rate vs. HP with Standard Deviation")
+plt.legend()
+plt.grid()
+
+# Reverse x-axis
+plt.xlim(max(hp_values), 1)
+
+plt.show()
