de027b45728d08e1c3846ac7c7147b9d407f3ec4
Santiago Ballerini <sballerini@itba.edu.ar>
2023-08-10 17:16:20 -0300

    feat: compute std_dev & use numpy

--- ej2-b.py
+++ ej2-b.py
@@ -1,18 +1,19 @@
 import csv
-from src.catching import attempt_catch
+import numpy as np
 from src.pokemon import PokemonFactory, StatusEffect
+from src.catching import attempt_catch
 
 # Config TODO: move to a config file?
 ball = "pokeball"
 pokemon_name = "snorlax"
 noise = 0.15
 iterations = 100
-output_file = "ej2-b.csv"
+output_file = "data/ej2-b.csv"
 
 # Experiment
-with open(output_file, 'w', newline='') as f:
+with open(output_file, "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["hp", "catch_rate"])
+    writer.writerow(["hp", "catch_rate", "std"])
 
     factory = PokemonFactory("pokemon.json")
     pokemon = factory.create(pokemon_name, 100, StatusEffect.NONE, 1)
@@ -20,9 +21,10 @@ with open(output_file, 'w', newline='') as f:
     for hp in range(1, pokemon.max_hp):
         pokemon.current_hp = hp
 
-        results = [attempt_catch(pokemon, ball, noise)[1] for _ in range(iterations)]
-        average = sum(results) / len(results)
+        results = np.array(
+            [attempt_catch(pokemon, ball, noise)[1] for _ in range(iterations)]
+        )
+        average = np.average(results)
+        std = np.std(results)
 
-        writer.writerow([hp, average])
-
-
+        writer.writerow([hp, average, std])
