b41fe3162971f76f6b164b6ac2e5db9415b98fbe
Franco Bosetti <francobosetti@live.com.ar>
2023-08-11 19:13:26 -0300

    feat: data folder now creates automatically

--- main.py
+++ main.py
@@ -12,30 +12,36 @@ if __name__ == "__main__":
 
     with open(config_file) as f:
         config = json.load(f)
+        data_path = config["data_path"]["path"]
+
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
 
         # Pokeball in ideal conditions, Ej 1-a
         experiment_config = config["pokeball_in_ideal_conditions_experiment"]
-        has_ran = os.path.isfile(experiment_config["csv"])
+        output_path = os.path.join(data_path, experiment_config["csv"])
+        has_ran = os.path.isfile(output_path)
 
         if not has_ran or experiment_config["rerun"]:
             print(
                 "Running Pokeball in ideal conditions Experiment, output file: "
-                + experiment_config["csv"]
+                + output_path
                 + "..."
             )
 
             pokeball_in_ideal_conditions_experiment(
-                output_file=experiment_config["csv"]
+                output_file=output_path
             )
 
         # Status Experiment, Ej 2-a
         experiment_config = config["variating_status_experiment"]
-        has_ran = os.path.isfile(experiment_config["csv"])
+        output_path = os.path.join(data_path, experiment_config["csv"])
+        has_ran = os.path.isfile(output_path)
 
         if not has_ran or experiment_config["rerun"]:
             print(
                 "Running Variating Status Experiment, output file: "
-                + experiment_config["csv"]
+                + output_path
                 + "..."
             )
 
@@ -45,17 +51,18 @@ if __name__ == "__main__":
                 level=experiment_config["level"],
                 hp_percentage=experiment_config["hp_percentage"],
                 iterations=experiment_config["iterations"],
-                output_file=experiment_config["csv"],
+                output_file=output_path,
             )
 
         # HP Experiment, Ej 2-b
         experiment_config = config["variating_hp_experiment"]
-        has_ran = os.path.isfile(experiment_config["csv"])
+        output_path = os.path.join(data_path, experiment_config["csv"])
+        has_ran = os.path.isfile(output_path)
 
         if not has_ran or experiment_config["rerun"]:
             print(
                 "Running Variating HP Experiment, output file: "
-                + experiment_config["csv"]
+                + output_path
                 + "..."
             )
 
@@ -64,22 +71,23 @@ if __name__ == "__main__":
                 ball=experiment_config["ball"],
                 noise=experiment_config["noise"],
                 iterations=experiment_config["iterations"],
-                output_file=experiment_config["csv"],
+                output_file=output_path,
             )
 
         # Everything Experiment, Ej 2-d
         experiment_config = config["variating_everything_experiment"]
-        has_ran = os.path.isfile(experiment_config["csv"])
+        output_path = os.path.join(data_path, experiment_config["csv"])
+        has_ran = os.path.isfile(output_path)
 
         if not has_ran or experiment_config["rerun"]:
             print(
                 "Running Variating Everything Experiment, output file: "
-                + experiment_config["csv"]
+                + output_path
                 + "..."
             )
 
             variating_everything_experiment(
                 pokemon_name=experiment_config["pokemon"],
                 iterations=experiment_config["iterations"],
-                output_file=experiment_config["csv"],
+                output_file=output_path,
             )
